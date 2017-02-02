9ebd4ff88fe4601e2ece0a874a456fb424f06a89
Ergin <mesarikaya@gmail.com>
2017-02-02 10:12:34 +0000

    Last commit

--- server.js
+++ server.js
@@ -5,21 +5,24 @@ var validUrl = require('url-valid');
 var api = require('./App/app.js');
 var cur=require("cur.lv")();
 
+
 // Connection URL
-var url = 'mongodb://localhost:27017/url_shortener_microservice';
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/url-shortener';
 
 mongo.connect(url, function(err,db){
     if (err) {
         throw err;
     }
+    
     // Create collection
     var options = {
-         server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
-         replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } 
+        server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+        replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } 
     };
+    
     db.createCollection("short_urls",options);
     
-    //Set the front page 
+    //Set the html 
     app.use(express.static(__dirname + "/../Public"));
     
     //call the app
